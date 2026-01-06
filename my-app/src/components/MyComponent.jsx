ffb6e8abac124ff1350311c2d3ab73d192645608
Minh3132004 <vubinhminh04@gmail.com>
2026-01-06 21:08:40 +0700

    Làm quen với State

--- my-app/src/components/MyComponent.jsx
+++ my-app/src/components/MyComponent.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 function MyComponent() {
+    const [state, setState] = useState({
+        name: 'Minh',
+        age: 22
+    });
+
     return (
         <div>
+            {state.name}, {state.age}-
             This is MyComponent
         </div>
     );
