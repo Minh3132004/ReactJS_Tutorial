722a6e39b68e07bf3d242a2ce267d4854347de2b
Minh3132004 <vubinhminh04@gmail.com>
2026-01-06 21:43:11 +0700

    Làm quen với DOM Events

--- my-app/src/components/MyComponent.jsx
+++ my-app/src/components/MyComponent.jsx
@@ -6,10 +6,16 @@ function MyComponent() {
         age: 22
     });
 
+    const handleClick = (event) => {
+        console.log(event.target);
+        console.log("My name is " + state.name + ", I'm " + state.age + " years old.");
+    };
+
     return (
         <div>
             {state.name}, {state.age}-
             This is MyComponent
+            <button onClick={handleClick} >Click me</button>
         </div>
     );
 }
