0ae36d53c30618ba89996321fbdef3f436d38a9d
Minh3132004 <vubinhminh04@gmail.com>
2026-01-11 08:35:58 +0700

    Design header

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
+import Header from './components/header/Header'
 
 function App() {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Hello, World!</h1>
-    </div>
+    </>
   )
 }
 
